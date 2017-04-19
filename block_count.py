f4b2f718c23fcf797f4d9d8c4cbac7a439f1dd9f
Ned Bingham <eab242@cornell.edu>
2017-04-19 14:56:10 -0400

    determines the block size of a given region and how many times that block was executed

---
+++ block_count.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+counts = dict()
+
+with open("astat.tsv", "r") as fptr:
+	for line in fptr:
+		splt = line.strip().split('\t')
+		addr = splt[0].lower()
+		count = splt[1].lower()
+
+		if count not in counts:
+			counts[count] = 0
+
+		counts[count] += 1
+		
+with open("acount.tsv", "w") as fptr:
+	for key,count in counts.items():
+		print >>fptr, key + "\t" + str(count)
