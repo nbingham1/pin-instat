30e6ddb88b4844e4ff2c04b1562860ef21b3e5d1
Ned Bingham <eab242@cornell.edu>
2017-04-19 20:49:36 -0400

    adding register lifespan

--- instat.cpp
+++ instat.cpp
@@ -18,6 +18,7 @@
 struct insrecord {
 	insrecord() {
 		count = 0;
+		mem_count = 0;
 		for (int i = 0; i < 64; i++)
 			bitwidth_count[i] = 0;
 	}
@@ -27,6 +28,7 @@ struct insrecord {
 	string category;
 	unsigned int bitwidth_count[64];
 	int count;
+	int mem_count;
 };
 
 struct addrrecord {
@@ -35,6 +37,33 @@ struct addrrecord {
 	}
 
 	int count;
+};
+
+std::map<int, int> regreadmap;
+
+struct regrecord {
+	regrecord() {
+		count = 0;
+	}
+
+	~regrecord() {}
+
+	int count;
+
+	void read() {
+		count++;
+	}
+
+	void write() {
+		if (count > 0) {
+			map<int, int>::iterator d = regreadmap.find(count);
+			if (d == regreadmap.end())
+				regreadmap[count] = 1;
+			else
+				d->second++;
+		}
+		count = 0;
+	}
 };
 
 enum
@@ -46,8 +75,11 @@ enum
 };
 
 const char *logname = "instat.log";
-const char *tsvname = "instat.tsv";
+const char *insname = "instat.tsv";
+const char *addrname = "addrstat.tsv";
+const char *regname = "regstat.tsv";
 FILE *logfp;
+std::map<string,regrecord> regmap;
 std::map<ADDRINT,addrrecord> addrmap;
 std::map<string,insrecord> insmap;
 std::map<ADDRINT,string> symbols;
@@ -121,20 +153,39 @@ static void on_ins (insrecord *rec, addrrecord *arec, UINT32 ops, ...)
 	va_list lst;
 	UINT32 bitwidth = 0;
 	ADDRINT addr, size, val;
+	UINT32 is_read = 0;
+	UINT32 is_write = 0;
+	regrecord *rrec = NULL;
+	bool is_mem = false;
 
 	va_start(lst, ops);
 	for (UINT32 i = 0; i < ops; i++) {
 		UINT32 type = va_arg(lst, UINT32);
 		switch (type) {
 		case iarg_reg:
+			rrec = va_arg(lst, regrecord*);
+			is_read = va_arg(lst, UINT32);
+			is_write = va_arg(lst, UINT32);
+			val = va_arg(lst, ADDRINT);
+			if (is_read) {
+				bitwidth = max(bitwidth, get_bitwidth(val));
+				rrec->read();
+			}
+
+			if (is_write) {
+				rrec->write();
+			}
+			break;
 		case iarg_imm:
-			bitwidth = max(bitwidth, get_bitwidth(va_arg(lst, ADDRINT)));
+			val = va_arg(lst, ADDRINT);
+			bitwidth = max(bitwidth, get_bitwidth(val));
 			break;
 		case iarg_mem:
 			addr = va_arg(lst, ADDRINT);
 			size = va_arg(lst, ADDRINT);
 			val = memory_getvalue(addr, size);
 			bitwidth = max(bitwidth, get_bitwidth(val));
+			is_mem = true;
 			break;
 		case iarg_br:
 		default:
@@ -145,6 +196,8 @@ static void on_ins (insrecord *rec, addrrecord *arec, UINT32 ops, ...)
 	rec->bitwidth_count[bitwidth]++;
 	rec->count++;
 	arec->count++;
+	if (is_mem)
+		rec->mem_count++;
 }
 
 static void instruction (INS ins, void *v)
@@ -163,13 +216,19 @@ static void instruction (INS ins, void *v)
 	UINT32 ops = 0;
 	for (UINT32 i = 0; i < INS_OperandCount(ins); i++) {
 		if (INS_OperandIsReg(ins, i)) {
-			if (INS_OperandRead(ins, i)) {
-				if (REG_is_integer(INS_OperandReg(ins, i))) {
-					IARGLIST_AddArguments(args, IARG_UINT32, iarg_reg,
-						IARG_REG_VALUE, INS_OperandReg(ins, i),
-						IARG_END);
-					ops++;
-				}
+			BOOL read = INS_OperandRead(ins, i);
+			BOOL write = INS_OperandWritten(ins, i);
+			string regname = REG_StringShort(INS_OperandReg(ins, i));
+			regrecord &rrecord = regmap[regname];	
+
+			if (REG_is_integer(INS_OperandReg(ins, i))) {
+				IARGLIST_AddArguments(args, IARG_UINT32, iarg_reg,
+					IARG_ADDRINT, &rrecord,
+					IARG_UINT32, (UINT32)read,
+					IARG_UINT32, (UINT32)write,
+					IARG_REG_VALUE, INS_OperandReg(ins, i),
+					IARG_END);
+				ops++;
 			}
 		} else if (INS_OperandIsImmediate(ins, i)) {
 			IARGLIST_AddArguments(args, IARG_UINT32, iarg_imm, IARG_ADDRINT, INS_OperandImmediate(ins, i), IARG_END);
@@ -199,11 +258,11 @@ static void instruction (INS ins, void *v)
 static void on_finish (INT32 code, void *v)
 {
 	fprintf(logfp, "finish %d\n", code);
-	FILE *fp = fopen(tsvname, "w");
-	for(std::map<string,insrecord>::iterator ite = insmap.begin(); ite != insmap.end(); ite++) {
+	FILE *fp = fopen(insname, "w");
+	for (std::map<string,insrecord>::iterator ite = insmap.begin(); ite != insmap.end(); ite++) {
 		struct insrecord &rec = ite->second;
 
-		fprintf(fp, "%s\t%s\t%d", rec.category.c_str(), ite->first.c_str(), rec.count);
+		fprintf(fp, "%s\t%s\t%d\t%d", rec.category.c_str(), ite->first.c_str(), rec.count, rec.mem_count);
 
 		for (int i = 0; i < 64; i++) {
 			fprintf(fp, "\t%u", rec.bitwidth_count[i]);
@@ -211,12 +270,21 @@ static void on_finish (INT32 code, void *v)
 
 		fprintf(fp, "\n");
 	}
-	fprintf(fp, "\n\n");
+	fclose(fp);
 
+	fp = fopen(addrname, "w");
 	for (std::map<ADDRINT, addrrecord>::iterator ite = addrmap.begin(); ite != addrmap.end(); ite++) {
 		fprintf(fp, PxPTR "\t%d\n", ite->first, ite->second.count);
 	}
+	fclose(fp);
 
+	fp = fopen(regname, "w");
+	for (std::map<string, regrecord>::iterator ite = regmap.begin(); ite != regmap.end(); ite++) {
+		ite->second.write();
+	}
+
+	for (std::map<int, int>::iterator i = regreadmap.begin(); i != regreadmap.end(); i++)
+		fprintf(fp, "%d\t%d\n", i->first, i->second);
 	fclose(fp);
 	fclose(logfp);
 }
